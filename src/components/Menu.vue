eb0351fcbe2dcc764570fac7ef19c383baf2ca58
caryolite <carl47063@gmail.com>
2026-04-24 15:39:03 +0800

    chore: menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,23 +3,25 @@
     <div class="mt-[180px] pr-45">
         <h2>歲歲今朝</h2>
         <p>home</p>
+        <span class="num1">1</span>
     </div>
     <div class="line1"></div>
     <div class="mt-10 pl-40">
         <h2>歲獸代理人</h2>
         <p>Character</p>
+        <span class="num2">2</span>
     </div>
     <div class="line2"></div>
     <div class="mt-10 pr-40">
         <h2>邸報</h2>
         <p>Story</p>
+        <span class="num3">3</span>
     </div>
     <div class="line3"></div>
     <div class="mt-60">
         <h2>後記</h2>
         <p>note</p>
     </div>
-    <!-- fuck -->
 </div>
 
 </template>
@@ -37,6 +39,8 @@ h2 {
 }
 p {
     text-align: center;
+    font-size: 10px;
+    color: var(--color-gray);
     /* margin: 0; */
 }
 .line1 {
@@ -56,5 +60,21 @@ p {
     margin: auto;
     border-top: 1px solid var(--color-red);
     transform: translateX(-40px) translateY(115px) rotate(70deg);
+}
+span {
+    position: absolute;
+    transform: translateX(60px);
+}
+.num1 {
+    color: var(--color-orange);
+    transform: translateX(160px) translateY(-60px);
+}
+.num2 {
+    color: var(--color-orange);
+    transform: translateX(180px) translateY(-60px);
+}
+.num3 {
+    color: var(--color-orange);
+    transform: translateX(85px) translateY(-60px);
 }
 </style>
